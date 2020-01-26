126bb4205d6f3664a3284e5d9cdc32cb5d557230
Ahsan-Masood <33608602+Ahsan-Masood@users.noreply.github.com>
2020-01-26 07:39:25 +0500

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,18 @@
+#Question no.1 - Write formula for Area of Circle.
+#Area of Circle = πr^2
+
+Radius = 5
+Pi = 3.14
+
+Area = Radius*(Pi*Pi)
+print (Area)
+
+#Question no.2 - Create lists.
+
+Cities = ["Faisalabad", "Islamabad", "Karachi", "Lahore", "Quetta"]
+Result = [93.4, 95, 98.7, 87.4, 83.02]
+Score = [45, 70, 54, 90, 42]
+
+print(Cities)
+print(Result)
+print(Score)
